8c9170db66c2bdef81d2a3d56e1214171601eb63
Frank Minichiello <9028036fmini@users.noreply.github.com>
2021-02-04 15:07:28 -0700

    Dissallowed '-' as a special character because it created a range in regex causing false positives for special chars

--- gens/specials.js
+++ gens/specials.js
@@ -1,5 +1,5 @@
 // character codes of allowed special characters
-specials = [33, 35, 36, 37, 38, 42, 43, 45, 63, 64, 94, 124];
+specials = [33, 35, 36, 37, 38, 42, 43, 63, 64, 94, 124];
 
 // randomly select a charCode from specials array
 const randSpec = () => {
